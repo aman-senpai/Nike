5a8d18010d26a8a82e714a17434e39a06983c8e9
aman-senpai <jemscollin1@gmail.com>
2023-09-18 21:20:02 +0530

    Create Sections Components

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+};
